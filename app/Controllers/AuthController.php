3b83cdaf4abd7097bf5f51733b7470e995aa0c8d
RAZAFINDRAVONONA Angeli Iriantsoa <iriantsoaangeli@gmail.com>
2026-07-20 11:36:56 +0300

    Fonction pour log in

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controllers;
 
-class Auth extends BaseController
+use App\Models\UserModel;
+
+class AuthController extends BaseController
 {
     public function login(): string
     {
-        return view('login');
+        return view('/login/login');
     }
 
-    public function seeTemplate(): string
+    public function authenticate($numero)
     {
-        return view('template');
+        $model = new UserModel();
+        if ($this->regex($numero)) {
+
+            if ($model->exists($numero) || $model->createUser($numero)) {
+                $user = $model->findByNumero($numero);
+                session()->set('numero', $user['numero']);
+                session()->set('isLoggedIn', true);
+            }
+            return redirect()->to('/operations/depot');
+        } else {
+            return redirect()->back()->with('error', 'Nummero invalide');
+        }
+    }
+
+    public function regex($numero)
+    {
+        return true;
     }
 }
